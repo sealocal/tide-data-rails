7ecd940d169bd9282082bb4d78fcdce7a9b87497
sealocal <local.mat@gmail.com>
2014-03-09 23:04:05 -0700

    rails new tide_data_rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TideDataRails::Application.config.secret_key_base = '3b7a29a030f49cd6d22163134ab9db4f03d44e9f47895cf0d68d43024ee63c870a6aa6c0e5809dccff52bfcf366579e8c7d0b55c07aa54ad156b201e8b5efc0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TideDataRails::Application.config.session_store :cookie_store, key: '_tide_data_rails_session'
