798b0efef7d52d0e4844000791d3c8346fa8325f
sealocal <local.mat@gmail.com>
2014-03-11 00:08:21 -0700

    add states page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 TideDataRails::Application.routes.draw do
-  get "states/index"
+  get "states" => 'states#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'states#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
