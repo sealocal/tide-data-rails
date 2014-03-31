a9ab620476f193999866bdf09f73423fa75d883b
sealocal <local.mat@gmail.com>
2014-03-31 10:05:57 -0700

    list xml filenames as regions

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -4,5 +4,13 @@ class StatesController < ApplicationController
 
   def show
     @state_name = params[:state_name]
+    @regions_array = Array.new
+    xml_dir = Rails.root.join 'app/xml/',  @state_name, 'regions'
+    xml_dir.each_entry do |file_path|
+      @regions_array << file_path.to_s
+    end
+    @regions_array.shift
+    @regions_array.shift
+    @regions_array.map! { |file_name| file_name.sub(".xml", "") }
   end
 end
